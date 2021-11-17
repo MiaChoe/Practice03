fc84849a5634b12ee18fb21d32ac3fe20c3a13ef
최승은 <liberalfilm@gmail.com>
2021-11-18 08:51:18 +0900

    2021.11.18(수)08:51

--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -1,11 +1,21 @@
-package com.javaex.practice;//Done//분석:true 값이면 그때 i를 출력하고 i는 1커진다, 1에서 2가 된k는 변수를 거짓으로 만듦. 아.
-									//i가 
-
+package com.javaex.practice;//Yet Done//분석: i가 1,2,3,4로 출력될 때까지만 작동=>틀렸어.5도 출력됨.
 public class Ex05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 int i = 1;
+		 boolean flag = true;
+		 
+		 while(flag) {
+			 if(i>=5) {
+				 flag = false;
+			 }
+			 System.out.println(i);
+			 i=i+1;
+			 
+		 }
+		
 	}
 
 }
---
+++ src/com/javaex/practice/Ex07.java
@@ -0,0 +1,13 @@
+package com.javaex.practice;//sc받은수 int num잡고 이게 사실상 x축이고 i변수랑 관계, int v를 세로축으로 잡아서
+							//int num = sc.nextInt();, for(i=1; (i<=num); i++0 { while (i=v) println} 이 형식 
+							//v가 i와 어떤 관계인지 설정을 해줘야 알 것
+public class Ex07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
