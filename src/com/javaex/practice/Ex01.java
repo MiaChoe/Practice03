a3d7ad46df7271aa382be951287323c165c36b5a
최승은 <liberalfilm@gmail.com>
2021-12-06 09:54:45 +0900

    2021.12.6(월)09:55

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -1,5 +1,5 @@
 package com.javaex.practice;//Done//출제포인트: while문과 for문의 표시법과 각각의 결과 출력 방법 보기. 두 변수는 for와 while 단어,변수 이름 빼고 둘은 같은 형태일 것이다=>옳다
-									//for문에서 조건문은 for문 열기 전 따로 선언하지 않고 for 안에서 최초로 자료형을 정해줘도 무방하구나
+									//for문에서 조건문은 for문 열기 전 따로 선언하지 않고 for 안에서 최초로 자료형을 정해줘도 무방함
 public class Ex01 {
 
 	public static void main(String[] args) {
