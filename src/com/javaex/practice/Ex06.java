743859c1a539f23c42b38f576b5f4d0006defe0f
최승은 <liberalfilm@gmail.com>
2021-12-14 01:29:22 +0900

    2021.12.14(화)01:28 for문 속 while문, &&조건

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -1,19 +1,24 @@
-package com.javaex.practice;//Done//출제포인트:조건문에&&를 쓰되 한계값 설정 적용하기(for문)
-							//안의 while문에 i++안 쓰니까 출력값35 goes infinite
+//Done//2021.11.18(목)/2021.12.14(화)01:27
+
+package com.javaex.practice;
 
 public class Ex06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int i;
-		for (i=1; (i>=1 && i<=100); i++) {
-			while ((i%5==0 && i%7==0)) {
-			System.out.println(i);
-			i++;
+		for (i = 1; (i >= 1 && i <= 100); i++) {
+			while ((i % 5 == 0 && i % 7 == 0)) {
+				System.out.println(i);
+				i++;
 			}
-		
+
 		}
 
 	}
 }
+
+/*
+2021.11.18(목) 출제포인트:조건문에&&를 쓰되 한계값 설정 적용하기(for문)안의 while문에 i++안 쓰니까 출력값35
+goes infinite
+*/
