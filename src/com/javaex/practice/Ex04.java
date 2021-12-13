06503c1e1dc7ec2c3a31dcad9c4130b359362416
최승은 <liberalfilm@gmail.com>
2021-12-14 01:12:02 +0900

    2021.12.14(화)01:14 Basic for문

--- src/com/javaex/practice/Ex04.java
+++ src/com/javaex/practice/Ex04.java
@@ -1,20 +1,26 @@
-package com.javaex.practice;//Done//예상:1,2,3,4 한줄에 하나 밑에 도트줄 밑에 10,8,6,4,2
+/*Done//2021.11.18(목)08:17 예상:1,2,3,4 한줄에 하나 밑에 도트줄 밑에 10,8,6,4,2/2021.12.14(화)01:12
+
+[문제] Ex04 아래의 출력결과를 예상하여 작성하세요.
+(코드를 작성해서 예상과 맞는지 확인해 보세요.)
+
+*/
+
+package com.javaex.practice;
 
 public class Ex04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		for(int i = 1; i<5; i++) {
+		for (int i = 1; i < 5; i++) {
 			System.out.println(i);
 		}
-		
+
 		System.out.println("================");
-		
-		for(int i=10; i>0; i=i-2) {
+
+		for (int i = 10; i > 0; i = i - 2) {
 			System.out.println(i);
 		}
-		
+
 	}
 
 }
