46a7fc2ca5288abecfcfe20522cd82b58296c948
최승은 <liberalfilm@gmail.com>
2021-12-14 02:17:46 +0900

    2021.12.14(화)02:19 미_구구단만들기 for문 속 for문, print(가로)와 println(세로) 섞어 쓰기

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,18 @@
+/*Undone//2021.12.14(화)02:04
+
+[문제] Ex08 아래와 같이 구구단을 출력하세요
+
+*/
+
+package com.javaex.practice;
+
+public class Ex08 {
+
+	int y = 1;
+	
+		for(x = 2; x < 10; x++){
+			System.out.print(x + "*" y + "=" + (x*y));
+		}
+		System.out.println("");
+	}
+}
